6ac7ca779c73d3e7c9620d4ba400a6e3d3ff73a7
Selim Enes Erdoğan <selim@Selim-M2-Macbook.local>
2023-06-13 17:21:36 +0300

    creators added

--- src/views/ComicDetailView.vue
+++ src/views/ComicDetailView.vue
@@ -34,6 +34,7 @@ const handleLike = () => {
 };
 
 onMounted(() => {
+  // Params üzerinden alınabilir.
   getSingleComic(window.location.pathname.split("/").reverse()[0]);
 });
 </script>
@@ -56,6 +57,45 @@ onMounted(() => {
             <p class="comicDetailDescription">
               {{ comicData[0].description || "No description" }}
             </p>
+            <div class="comicDetailCreators">
+              <h3>Creators</h3>
+              <div class="comicDetailCreatorsWrapper">
+                <p v-if="comicData[0].creators.items.length === 0">
+                  No creator info.
+                </p>
+                <a
+                  v-else
+                  v-for="creator in comicData[0].creators.items"
+                  target="_blank"
+                  :key="creator.resourceURI"
+                  :href="creator.resourceURI"
+                  class="comicDetailCreator"
+                >
+                  {{ creator.name }}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    height="800px"
+                    width="800px"
+                    version="1.1"
+                    id="Capa_1"
+                    viewBox="0 0 26 26"
+                    xml:space="preserve"
+                  >
+                    <g>
+                      <path
+                        d="M18,17.759v3.366C18,22.159,17.159,23,16.125,23H4.875C3.841,23,3,22.159,3,21.125V9.875   C3,8.841,3.841,8,4.875,8h3.429l3.001-3h-6.43C2.182,5,0,7.182,0,9.875v11.25C0,23.818,2.182,26,4.875,26h11.25   C18.818,26,21,23.818,21,21.125v-6.367L18,17.759z"
+                      />
+                      <g>
+                        <path
+                          d="M22.581,0H12.322c-1.886,0.002-1.755,0.51-0.76,1.504l3.22,3.22l-5.52,5.519    c-1.145,1.144-1.144,2.998,0,4.141l2.41,2.411c1.144,1.141,2.996,1.142,4.14-0.001l5.52-5.52l3.16,3.16    c1.101,1.1,1.507,1.129,1.507-0.757L26,3.419C25.999-0.018,26.024-0.001,22.581,0z"
+                        />
+                      </g>
+                    </g>
+                  </svg>
+                </a>
+              </div>
+            </div>
             <div class="comicDetailCharacters">
               <h3>Characters</h3>
               <div class="comicDetailCharactersWrapper">
@@ -162,6 +202,38 @@ onMounted(() => {
       .comicDetailDescription {
         font-size: 1.2rem;
         margin-bottom: 1rem;
+      }
+
+      .comicDetailCreators {
+        margin-bottom: 2rem;
+
+        h3 {
+          font-size: 1.5rem;
+          margin-bottom: 1rem;
+        }
+
+        .comicDetailCreatorsWrapper {
+          display: flex;
+          flex-wrap: wrap;
+          gap: 1rem;
+
+          .comicDetailCreator {
+            color: inherit;
+            text-decoration: none;
+            display: flex;
+            gap: 0.2rem;
+
+            svg {
+              fill: white;
+              width: 0.7rem;
+              height: 0.7rem;
+            }
+
+            &:hover {
+              text-decoration: underline;
+            }
+          }
+        }
       }
 
       .comicDetailCharacters {
